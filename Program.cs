afe82615f359c0c82ba090af733ee9e44a7b398d
Andrei <andruxametelkin@gmail.com>
2022-06-30 02:29:31 +0400

    update homework

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /*Zadacha 10
+void FindSecondDigit()
+{
+    Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 | number >= 1000 )
+{
+    Console.WriteLine("Число не является трехзначным");
+}
+else 
+{
+int des = number / 10;
+int two = des % 10;
+Console.WriteLine("Второе цифра данного числа = " + two);
+}
+}
+FindSecondDigit();
+*/ 
+
+/* Zadacha 13 */
+
+void FindThirdDigit()
+{
+    
+    int rand = new Random().Next();
+    Console.WriteLine("Заданное число = " + rand);
+    if (rand < 100)
+    {
+        Console.WriteLine("У числа нет третьей цифры");
+    }
+    else
+    {
+        Console.WriteLine(rand.ToString()[2]);
+       /* int third = 0;
+        while (rand < 100)
+     {
+         third = rand % 10;
+         rand = rand / 10;
+         Console.WriteLine("Заданное число = " + rand); 
+     }
+     Console.WriteLine("Второе цифра данного числа = " + third); */   
+    }
+}
+FindThirdDigit();
+
