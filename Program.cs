e8b00391003d599aad29f28847d37317d480df81
Andrei <andruxametelkin@gmail.com>
2022-06-30 20:08:59 +0400

    Complete Homework_2

--- Program.cs
+++ Program.cs
@@ -61,3 +61,23 @@ void FindThirdDigit()
 }
 FindThirdDigit(); */
 
+/* Zadacha 15 
+void Determiningtheday()
+{
+    Console.Write("Введите число");
+ int number = Convert.ToInt32(Console.ReadLine());
+    if (number < 1 | number > 7)
+    {
+        Console.WriteLine("Неверно введено число, повторите попытку");
+    }
+     if (number >= 1 && number <= 5)
+    {
+      Console.WriteLine("Данный день недели не является выходным");
+    }
+     if (number >= 6 && number <= 7)
+    {
+      Console.WriteLine("Данный день недели является выходным");
+    }
+}
+Determiningtheday();
+*/
